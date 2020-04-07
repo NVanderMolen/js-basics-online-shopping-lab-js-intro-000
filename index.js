4e656feaf9eda6b985be04bbbedc45643440ebc1
Nick VanderMolen <tree.vandermolen@gmail.com>
2020-04-07 13:37:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -50,8 +50,7 @@ function removeFromCart(item) {
 
  function placeOrder(cardNumber) {
     if (!cardNumber) {
-      return "Sorry, we don't have a credit card on file for you."
-   }
-     cart.splice(0, cart.length);
-     return `Your total cost is ${total().}, which will be charged to the card ${cardNumber}.`
+        return "Sorry, we don't have a credit card on file for you."
+   } cart.splice(0, cart.length);
+        return `Your total cost is ${total().cartTotal}, which will be charged to the card ${cardNumber}.`
    }
